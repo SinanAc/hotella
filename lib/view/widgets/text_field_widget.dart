e200c3d8ea590f2eb8497176ebd3f96b72dac5fb
Sinan <sinanac124@gmail.com>
2022-11-08 21:17:36 +0530

    refactoring

--- lib/view/widgets/text_field_widget.dart
+++ lib/view/widgets/text_field_widget.dart
@@ -1,35 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:premio_inn/utils/colors.dart';
 
-class TextFieldWidget extends StatelessWidget {
-  final String hintText;
-  final TextEditingController controller;
-  final String? Function(String?)? validator;
-  final TextInputType keyType;
-  const TextFieldWidget({
+class TextFieldWidget extends TextFormField {
+  TextFieldWidget({
     Key? key,
-    required this.hintText,
-    required this.controller,
-    required this.validator,
-    this.keyType = TextInputType.emailAddress,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      validator: validator,
-      decoration: InputDecoration(
-        border: const OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(15.0)),
-        ),
-        fillColor: KColors.kTextFieldColor,
-        filled: true,
-        hintText: hintText,
-        hintStyle: const TextStyle(color: KColors.kGreyColor),
-      ),
-      keyboardType: keyType,
-      style: const TextStyle(),
-    );
-  }
+    required String hintText,
+    required TextEditingController controller,
+    required String? Function(String?)? validator,
+    TextInputType keyType = TextInputType.emailAddress,
+  }) : super(
+          key: key,
+          controller: controller,
+          validator: validator,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+            ),
+            fillColor: KColors.kTextFieldColor,
+            filled: true,
+            hintText: hintText,
+            hintStyle: const TextStyle(color: KColors.kGreyColor),
+          ),
+          keyboardType: keyType,
+        );
 }
