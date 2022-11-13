fa64727c93fe867e3b9dcb6c4e2b893a3d986830
Sinan <sinanac124@gmail.com>
2022-11-13 17:33:24 +0530

    booked rooms service

--- lib/view/screens/bookings/widgets/coming_screen.dart
+++ lib/view/screens/bookings/widgets/coming_screen.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:premio_inn/utils/sizes.dart';
+import 'package:premio_inn/view/widgets/loading_indicator.dart';
+import 'package:premio_inn/view_model/booked/bookings.dart';
+import 'package:provider/provider.dart';
 
 class ComingScreen extends StatelessWidget {
   const ComingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        itemCount: 8,
+    final bookingsPro = context.read<BookedHotelsViewModel>();
+    return
+    FutureBuilder(
+      future: bookingsPro.getAllBookedHotels(),
+      builder:((_, __) {
+      return
+      bookingsPro.isLoading?
+    const LoadingIndicator():
+    bookingsPro.bookedHotels.isEmpty?
+    const Center(child: Text('No records found')):
+     ListView.separated(
+        itemCount: bookingsPro.bookedHotels.length,
         itemBuilder: ((_, index) {
-          return const SizedBox();
-          // MainCard(
-          //   name: 'Vistara egmondo',
-          //   loaction: 'Genevia',
-          //   img: 'assets/images/hotel_dummy.jpg',
-          //   size: MediaQuery.of(context).size,
-          // );
+          return 
+          const SizedBox(
+            width: 100,
+            child:  ColoredBox(color: Colors.black,
+            child: SizedBox(height: 100,),
+            ),
+          );
         }),
         separatorBuilder: ((_, __) {
           return KSizedBox.kHeigh_10;
         }));
+    }));
   }
 }
--- lib/view_model/booked/bookings.dart
+++ lib/view_model/booked/bookings.dart
@@ -1,24 +1,42 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:premio_inn/model/booked/booked_rooms.dart';
 import 'package:premio_inn/services/booked/booked_rooms.dart';
 
 class BookedHotelsViewModel extends ChangeNotifier {
-  final bool _isLoading = false;
+  BookedHotelsViewModel(){
+    getAllBookedHotels();
+  }
+  bool _isLoading = false;
   bool get isLoading => _isLoading;
   final List<Completed> bookedHotels = [];
   Future<void> getAllBookedHotels() async {
+    _isLoadingTrue();
     final BookedRoomsModel? response = await BookedRoomsService().bookedRooms();
     if (response == null) {
+      _isLoadingFalse();
       return;
     } else if (response.success == true) {
       bookedHotels.clear();
       bookedHotels.addAll(response.completed ?? []);
-      notifyListeners();
+     // notifyListeners();
+      _isLoadingFalse();
+      return;
     } else {
       log('no data');
-      log(response.success.toString());
+      _isLoadingFalse();
     }
+  }
+
+    // ==========>>>>>  TO MAKE LOADING TRUE  <<<<<==========
+  void _isLoadingTrue() {
+    _isLoading = true;
+   // notifyListeners();
+  }
+
+  // ==========>>>>>  TO MAKE LOADING FALSE  <<<<<==========
+  void _isLoadingFalse() {
+    _isLoading = false;
+    //notifyListeners();
   }
 }
